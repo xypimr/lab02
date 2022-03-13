c96e82032a013c4e17707c16533c3a00bca0cc41
xypimr <kiselev.nik.v@gmail.com>
2022-03-13 20:48:49 +0300

    Added clang-format for hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
-
-int main(){
-    std::string name; 
-    std::cin >> name; // Inputing name from console
-    std::cout << "Hello world from " << name << std::endl; // Outputing text with name
-    return 0; 
+int
+main()
+{
+  std::string name;
+  std::cin >> name; // Inputing name from console
+  std::cout << "Hello world from " << name
+            << std::endl; // Outputing text with name
+  return 0;
 }
